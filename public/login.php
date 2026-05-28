2006a49ba52e0a4f05ec02b0bfb54b83329ffd7a
clarapereira1240902 <1240902@isep.ipp.pt>
2026-05-28 15:18:13 +0100

    Ficha10.01 Formulário de Autenticação

--- public/login.php
+++ public/login.php
@@ -1,26 +1,7 @@
-<!DOCTYPE html>
-<html lang="pt">
+<?php include '../private/includes/header.php'; ?>
 
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>ISEP Ginásio</title>
 
-    <!-- Bootstrap CSS & custom CSS --> 
-    <link rel="stylesheet" href="../frontend/assets/bootstrap/bootstrap.min.css"> 
-    <link rel="stylesheet" href="../frontend/assets/css/estilos.css"> 
 
-    <!-- favicon --> 
-    <link rel="shortcut icon" href="../frontend/assets/img/gym125.png" type="image/png"> 
-
-    <!-- Font Awesome --> 
-    <link rel="stylesheet" href="../frontend/assets/fontawesome/all.min.css"> 
-
-    <!-- Google Fonts --> 
-    <link href="https://fonts.googleapis.com/css2?family=Titillium+Web:wght@300;700&display=swap" rel="stylesheet"> 
-</head>
-
-<body>
     <div class="container-fluid mt-5">
         <div class="row justify-content-center">
             <div class="col-lg-5 col-md-6 col-sm-8 col-10">
@@ -29,8 +10,8 @@
  
                     <div class="d-flex align-items-center justify-content-center my-4"> 
                         <!-- Imagem do ginásio + texto -->
-                        <img src="../frontend/assets/img/gym125.png"> 
-                        <h2><strong>ISEP - Ginásio</strong></h2>  
+                        <img src="/isep-ginasio/private/assets/img/gym125.png" class="img-fluid me-3"> 
+                        <h2><strong> <?php echo APP_NAME; ?></strong></h2>  
                     </div> 
  
                     <div class="row"> 
@@ -74,10 +55,4 @@
 
 
 
-
-
-    <!-- Bootstrap JS and custom JS --> 
-    <script src="assets/bootstrap/bootstrap.bundle.min.js"></script> 
-</body>
-
-</html>
+<?php include '../private/includes/footer.php'; ?> 
